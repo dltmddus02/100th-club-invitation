afc06d3b3e2e13b00954f646e3c7a5a78f73c329
dltmddus <tmddus4671@gmail.com>
2024-07-17 15:55:42 +0900

    feat: banner 기능 추가

---
+++ banner.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // DOM이 완전 로드되고 파싱된 후에 진행...
+  const outer = document.querySelector(".outer");
+  const innerList = document.querySelector(".inner-list");
+  const inners = document.querySelectorAll(".inner");
+  let currentIndex = 0;
+
+  inners.forEach((inner) => {
+    inner.style.width = `${outer.clientWidth}px`;
+  });
+
+  innerList.style.width = `${outer.clientWidth * inners.length}px`;
+
+  // 버튼에 이벤트 등록하기
+  const buttonLeft = document.querySelector(".button-left");
+  const buttonRight = document.querySelector(".button-right");
+
+  buttonLeft.addEventListener("click", () => {
+    currentIndex--;
+    currentIndex = currentIndex < 0 ? 0 : currentIndex;
+    innerList.style.marginLeft = `-${outer.clientWidth * currentIndex}px`;
+    clearInterval(interval);
+    interval = getInterval();
+    console.log("왼쪽버튼클릭");
+  });
+
+  buttonRight.addEventListener("click", () => {
+    currentIndex++;
+    currentIndex =
+      currentIndex >= inners.length ? inners.length - 1 : currentIndex;
+    innerList.style.marginLeft = `-${outer.clientWidth * currentIndex}px`;
+    clearInterval(interval);
+    interval = getInterval();
+  });
+
+  // 주기적으로 화면 넘기기
+  const getInterval = () => {
+    return setInterval(() => {
+      currentIndex++;
+      currentIndex = currentIndex >= inners.length ? 0 : currentIndex;
+      innerList.style.marginLeft = `-${outer.clientWidth * currentIndex}px`;
+    }, 2000); // 2s 간격
+  };
+
+  let interval = getInterval();
+});
