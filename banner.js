45bcbd744f70545ceca5d17e0dc1e161f740069d
dltmddus <tmddus4671@gmail.com>
2024-07-17 16:46:53 +0900

    feat: 베너 크기 반응형 조절

--- banner.js
+++ banner.js
@@ -1,15 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // DOM이 완전 로드되고 파싱된 후에 진행...
+  //   const realSize = window.innerWidth;
+  //   document.body.style.width = `${realSize}px`;
   const outer = document.querySelector(".outer");
   const innerList = document.querySelector(".inner-list");
   const inners = document.querySelectorAll(".inner");
   let currentIndex = 0;
 
-  inners.forEach((inner) => {
-    inner.style.width = `${outer.clientWidth}px`;
-  });
+  const updateSizes = () => {
+    const realSize = window.innerWidth;
+    document.body.style.width = `${realSize}px`;
+    inners.forEach((inner) => {
+      inner.style.width = `${outer.clientWidth}px`;
+    });
+    innerList.style.width = `${outer.clientWidth * inners.length}px`;
+  };
 
-  innerList.style.width = `${outer.clientWidth * inners.length}px`;
+  // 초기 설정
+  updateSizes();
+
+  // 창 크기 조절할 때마다 실행
+  window.addEventListener("resize", updateSizes);
 
   // 버튼에 이벤트 등록하기
   const buttonLeft = document.querySelector(".button-left");
