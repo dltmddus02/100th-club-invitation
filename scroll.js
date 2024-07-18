fa3734124cbd6210af8f7ca7199824e0eb0103aa
dltmddus <tmddus4671@gmail.com>
2024-07-18 16:16:41 +0900

    fix: 이미지 경로 수정

--- scroll.js
+++ scroll.js
@@ -1,11 +1,11 @@
 $(window).on("scroll", function () {
   if ($(this).scrollTop() > 100) {
-    $("#scroll-image, #firework-1, #firework-2")
+    $("#scroll-image, #fireworks-1, #fireworks-2")
       .addClass("show")
       .removeClass("hide");
   } else {
     setTimeout(function () {
-      $("#scroll-image, #firework-1, #firework-2")
+      $("#scroll-image, #fireworks-1, #fireworks-2")
         .addClass("show")
         .removeClass("hide");
     }, 2000);
