2f14caf175a0699eb6fa34c2f79d72e8dd196e37
dltmddus <tmddus4671@gmail.com>
2024-07-18 16:13:28 +0900

    fix: 폭죽 이미지 오류 수정

--- scroll.js
+++ scroll.js
@@ -1,9 +1,13 @@
 $(window).on("scroll", function () {
   if ($(this).scrollTop() > 100) {
-    $("#scroll-image").fadeIn();
+    $("#scroll-image, #firework-1, #firework-2")
+      .addClass("show")
+      .removeClass("hide");
   } else {
     setTimeout(function () {
-      $("#scroll-image").fadeOut();
+      $("#scroll-image, #firework-1, #firework-2")
+        .addClass("show")
+        .removeClass("hide");
     }, 2000);
   }
 });
