738bb5905c7691f879dcccf14e6316519d91b31b
dltmddus <tmddus4671@gmail.com>
2024-07-19 14:30:49 +0900

    feat: 스크롤시 폭죽 터뜨리기 추가

--- scroll.js
+++ scroll.js
@@ -26,55 +26,24 @@ $(window).on("wheel", function (e) {
   var posTop = (page - 1) * $(window).height();
   $html.animate({ scrollTop: posTop });
 });
-// let num = 0;
-// let idx = 0;
-// let delta;
 
-// const MainTit = document.querySelector(".main-tit-1");
+$(document).ready(function () {
+  var lastScrollTop = 0;
+  var elements = $("#scroll-image, #firework");
 
-// $(window).on("scroll", function () {
-//   if ($(this).scrollTop() > 100) {
-//     MainTit.style.display = "block";
-//   } else {
-//     MainTit.style.display = "none";
-//   }
-// });
-
-// function handleWheel(e) {
-//   var delta = e.deltaY || e.detail || e.wheelDalta;
-//   handleScroll(delta);
-// }
-
-// function handleTouch(e) {
-//   if (e.type == "touchstart" && e.touches && e.touches[0]) {
-//     touchStartY = e.touches[0].clientY;
-//   } else if (e.type == "touchmove" && e.touches && e.touches[0]) {
-//     var touchEndY = e.touches[0].clientY;
-//     var delta = touchStartY - touchEndY;
-//     touchStartY = touchEndY;
-//     handleScroll(delta);
-//   }
-// }
-
-// function handleScroll(delta) {
-//   if (delta > 0) {
-//     // 스크롤 아래로 내릴 때
-//     if (!(num == 12)) {
-//       num++;
-//     }
-//   }
-// }
-
-// $(window).on("scroll", function () {
-//   var elements = $("#scroll-image, #firework");
-//   if ($(this).scrollTop() > 100) {
-//     elements.addClass("show").removeClass("hide");
-//     setTimeout(function () {
-//       elements.addClass("hide").removeClass("show");
-//     }, 2000);
-//   } else {
-//     setTimeout(function () {
-//       elements.addClass("hide").removeClass("show");
-//     }, 2000);
-//   }
-// });
+  $(window).on("scroll", function () {
+    var scrollTop = $(this).scrollTop();
+    if (scrollTop > lastScrollTop) {
+      elements.addClass("show").removeClass("hide");
+      setTimeout(function () {
+        elements.addClass("hide").removeClass("show");
+      }, 2000);
+    }
+    // else {
+    // setTimeout(function () {
+    // elements.addClass("hide").removeClass("show");
+    // }, 2000);
+    // }
+    lastScrollTop = scrollTop;
+  });
+});
